51a6142c9e02db54bdd47b0d8ecd022c794b4d48
John Sanchez <jostsace@estudiantec.cr>
2023-10-14 18:35:48 -0600

    Prueba Console log js

--- main.js
+++ main.js
@@ -5,4 +5,5 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
+  console.log("prueba");
 }
