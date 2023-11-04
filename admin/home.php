87b34c9875bfc8754d8367c383a7059d4662fd75
mauricio <mauriciocamposc20@gmail.com>
2023-11-04 14:58:13 -0600

    Tablas y home

--- admin/home.php
+++ admin/home.php
@@ -7,8 +7,33 @@ require 'inc/header.php';
 
 ?>
 
+    <div class="admin-panel">
+        <div class="container-fluid w-100">
+            <h1>Panel de administrador</h1>
+            <h6>Bienvenido, administrador</h6>
+        </div>
+        <div class="container" style="background-color: lightgray;">
+            <div class="row">
+                <div class="col-sm-12" style="margin-bottom: 10px;">
+                    <div class="pull-left">
+                        <a href="./users.php" style="width: 100%; height: 100px;" class="btn btn-outline-dark btn-user-background"><i class="fa fa-book fa-fw" aria-hidden="true"></i> Boton1 </a>
+                    </div>
+                    <div class="">
+                        <a href="./events.php" style="width: 100%; height: 100px;" class="btn btn-outline-dark btn-event-background"><i class="fa fa-book fa-fw" aria-hidden="true"></i> Boton2 </a>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="pull-left">
+                        <a href="./bills.php" style="width: 100%; height: 100px;" class="btn btn-outline-dark btn-bill-background"><i class="fa fa-book fa-fw" aria-hidden="true"></i>  Boton3 </a>
+                    </div>
+                    <div class="">
+                        <a href="menu.php" style="width: 100%; height: 100px;" class="btn btn-outline-dark btn-menu-background"><i class="fa fa-book fa-fw" aria-hidden="true"></i> Boton4 </a>
+                    </div>
+                </div>
+            </div>
+        </div>      
+    </div>
 
-<h1>Panel de Administración</h1>
 
 
 <?php require 'inc/footer.php'; ?>
