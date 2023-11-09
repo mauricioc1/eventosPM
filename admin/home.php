2484a47221ca89f44cc9a9e65fba1afea1adb2ed
mauricio <mauriciocamposc20@gmail.com>
2023-11-08 19:36:46 -0600

    Navegación con tabIndex
    Se agregó el atributo tabindex donde era necesario para una navegación adecuada, además de corregirse la palabra misión

--- admin/home.php
+++ admin/home.php
@@ -9,30 +9,30 @@
     <div class="panel-container">
         <div class="admin-panel">
             <div class="admin_panel_header">
-                <h1>Panel de administrador</h1>
-                <button class="btn_logout" data-logout="true">Cerrar Sesión</button>
+                <h1 tabindex="1">Panel de administrador</h1>
+                <button class="btn_logout" data-logout="true" tabindex="2">Cerrar Sesión</button>
             </div>
 
             <div class="panel-navigation-container">
-                <a href="users.php" class="navigation-option">
+                <a href="users.php" class="navigation-option" tabindex="3">
                     <div>
                         <p>Administrar Usuarios</p>
                         <i class="fa-regular fa-circle-user"></i>
                     </div>
                 </a>
-                <a href="events.php" class="navigation-option">
+                <a href="events.php" class="navigation-option" tabindex="4">
                     <div >
                         <p>Administrar Eventos</p>
                         <i class="fa-regular fa-calendar-days"></i>
                     </div>
                 </a>
-                <a href="bills.php" class="navigation-option option-below">
+                <a href="bills.php" class="navigation-option option-below" tabindex="5">
                     <div >
                         <p>Administrar Facturas</p>
                         <i class="fa-solid fa-file-invoice-dollar"></i>
                     </div>
                 </a>
-                <a href="menu.php" class="navigation-option option-below">
+                <a href="menu.php" class="navigation-option option-below" tabindex="6">
                     <div>
                         <p>Administrar Menú</p>
                         <i class="fa-solid fa-utensils"></i>
--- admin/index.php
+++ admin/index.php
@@ -11,23 +11,23 @@
 <div class="background_login">
 
     <div class="login_container">
-        <div class="login_logo"><img src="../imgs/logo.png" alt="logo EventosPM"></img></div>
-        <h1>Ingresar al panel de administrador</h1>
+        <div class="login_logo"><img src="../imgs/logo.png" alt="logo EventosPM" tabindex="1"></img></div>
+        <h1 tabindex="2">Ingresar al panel de administrador</h1>
         
         <form id="login_admin" method="post">
 
             <div class="campo">
                 <label for="email"><i class="fa-regular fa-circle-user"></i></label>
-                <input type="email" name="email" id="email" placeholder="Correo Electrónico">
+                <input type="email" name="email" id="email" placeholder="Correo Electrónico" tabindex="3">
             </div>
 
             <div class="campo">
                 <label for="password"><i class="fa-solid fa-lock"></i></label>
-                <input type="password" name="password" id="password" placeholder="Contraseña">
+                <input type="password" name="password" id="password" placeholder="Contraseña" tabindex="4">
             </div>
 
             <div class="submit_btn">
-                <input type="submit" value="Iniciar sesión">
+                <input type="submit" value="Iniciar sesión" tabindex="5">
             </div>
 
 
